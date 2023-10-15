1b98d80836832a8e49c5653f515db0f75c223dd6
BilalThaha <bilal.athaha@gmail.com>
2023-10-15 11:09:48 +0530

    neetcode 75 commit

---
+++ src/main/java/com/interview/neetcode75/ContainsDuplicate.java
@@ -0,0 +1,54 @@
+package com.interview.neetcode75;
+
+import java.util.HashSet;
+
+/**
+ * Given an integer array nums, return true if any value appears at least twice
+ * in the array, and return false if every element is distinct.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * Input: nums = [1,2,3,1]
+ * Output: true
+ * Example 2:
+ * 
+ * Input: nums = [1,2,3,4]
+ * Output: false
+ * Example 3:
+ * 
+ * Input: nums = [1,1,1,3,3,4,3,2,4,2]
+ * Output: true
+ * 
+ * 
+ * Constraints:
+ * 
+ * 1 <= nums.length <= 105
+ * -109 <= nums[i] <= 109
+ **/
+public class ContainsDuplicate {
+
+    public Boolean checkDuplicate(int[] numArr) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : numArr) {
+            if (set.contains(num)) {
+                return false;
+            } else {
+                set.add(num);
+            }
+        }
+        return true;
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = { 1, 2, 3, 1 };
+        int[] arr2 = { 1, 2, 3, 4 };
+        int[] arr3 = { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 };
+        ContainsDuplicate cd = new ContainsDuplicate();
+        System.out.println(cd.checkDuplicate(arr1));
+        System.out.println(cd.checkDuplicate(arr2));
+        System.out.println(cd.checkDuplicate(arr3));
+    }
+}
