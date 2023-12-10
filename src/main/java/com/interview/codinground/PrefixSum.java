c36dae3d47ca9734470879416c435234b065d7d0
BilalThaha <bilal.athaha@gmail.com>
2023-12-10 17:48:30 +0530

    Fixed exception

--- src/main/java/com/interview/codinground/PrefixSum.java
+++ src/main/java/com/interview/codinground/PrefixSum.java
@@ -5,13 +5,16 @@ public class PrefixSum {
         int[] arr ={1,2,3,4,5};
         int[] sumArr = computePrefixSum(arr);
         //System.out.println(sumArr);
+        for(int num: sumArr){
+            System.out.println(num);
+        }
     }
 
     private static int[] computePrefixSum(int[] arr) {
         int[] prefixSum = new int[arr.length];
         prefixSum[0] = arr[0];
 
-        for(int i=0;i<arr.length; i++){
+        for(int i=1;i<arr.length; i++){
             prefixSum[i] = prefixSum[i-1]+arr[i];
         }
         return prefixSum;
