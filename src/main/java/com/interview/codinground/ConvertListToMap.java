cb6d7fcf50c3a502dbac384bf7fa35d44cd024f4
BilalThaha <bilal.athaha@gmail.com>
2024-02-16 11:02:28 +0530

    commit

--- src/main/java/com/interview/codinground/ConvertListToMap.java
+++ src/main/java/com/interview/codinground/ConvertListToMap.java
@@ -15,12 +15,13 @@ public class ConvertListToMap {
         student.add(new StudentDetail("001", "John", "A", 10));
         student.add(new StudentDetail("002", "Alice", "A", 67));
         student.add(new StudentDetail("003", "Bob", "B", 46));
-        // Convert list to map with roll no as key and emp object as value
+        // Convert list to map with roll no as key and student object as value
         Map<String, StudentDetail> studentMap = student.stream()
                 .collect(Collectors.toMap(StudentDetail::getRollno, st -> st));
 
         studentMap.forEach((key, value) -> System.out.println(key + ", " + value));
 
+        // Convert list to map with Division as key and list pf student object as value
         Map<String, List<StudentDetail>> studentDivisionMap = student.stream()
                 .collect(Collectors.groupingBy(StudentDetail::getDivision));
 
